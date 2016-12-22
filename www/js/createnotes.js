e1b6d960fea12f3e803ba9c8768a4e3c6ec294ad
Kyle St-Hill <kylesthill77@gmail.com>
2016-12-22 14:55:22 -0500

    working create notes

--- www/js/createnotes.js
+++ www/js/createnotes.js
@@ -135,7 +135,7 @@ var app={
 			app.createNewPasswordForInsert();
 		}
 	},
-		
+
 	checkForPasswordDelete:function(){
 		var password = localStorage.getItem('appPsss21');
 		var idx = document.URL;
@@ -178,7 +178,8 @@ var app={
      	var desc = document.getElementById("topicdesc").value;
 		//encrpt here and store in var		
 		if(topicname.length == 0 && desc.length == 0){
-			Materialize.toast('Empty fields', 4000);
+			//Materialize.toast('Empty fields', 4000);
+			$('.emptyfields').stop().fadeIn(400).delay(3000).fadeOut(400);
 			return false;
 		}else if(topicname!=null && desc!=null){
 			//check that topic does not already exist
@@ -190,7 +191,7 @@ var app={
     		tx.executeSql('SELECT * from topics WHERE topic=?',[topicnametobechecked],function(tx,results){
     			var len = results.rows.length, i;
     			if(len>0){
-    				Materialize.toast('Topic exist already', 4000);
+    				$('.topicalreadyexist').stop().fadeIn(400).delay(3000).fadeOut(400);
 					return false;
     			}else{
 					desctobeadded = CryptoJS.AES.encrypt(desctobeadded,SECRET_PHRASE);
